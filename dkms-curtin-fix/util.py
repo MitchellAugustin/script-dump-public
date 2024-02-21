96c35af996fef83bbbaba0fa8d1415d4b9073106
Mitchell Augustin <mitchell.augustin@canonical.com>
2024-02-21 09:12:13 -0600

    Binding true from within target environment

--- dkms-curtin-fix/util.py
+++ dkms-curtin-fix/util.py
@@ -779,8 +779,9 @@ class ChrootableTarget(object):
 
         # Symlink true to ischroot since we may be in separate PID
         # namespace, which can throw off ischroot
+        true_mount_path = paths.target_path(self.target, '/usr/bin/true')
         ischroot_mount_path = paths.target_path(self.target, '/usr/bin/ischroot')
-        if do_mount('/usr/bin/true', ischroot_mount_path, opts='--bind'):
+        if do_mount(true_mount_path, ischroot_mount_path, opts='--bind'):
             self.umounts.append(ischroot_mount_path)
 
         return self
