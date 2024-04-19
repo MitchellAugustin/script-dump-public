33e8ad48bb17345723b476b785b3777eab6ae0e8
Mitchell Augustin <mitchell.augustin@canonical.com>
2024-04-19 10:06:15 -0500

    Changed check to ready

--- maas-alert-on-machine-ready/alert-snippet.js
+++ maas-alert-on-machine-ready/alert-snippet.js
@@ -1,7 +1,7 @@
 //Paste this snippet into your inspect element console with the page of the machine you're watching open, and it will alert you when the machine is
 function checkCondition() {
     const element = document.getElementsByClassName("u-nudge-left").item(0);
-    if (element && element.textContent.trim() === 'Releasing') {
+    if (element && element.textContent.trim() === 'Ready') {
         (new Audio("https://oneone.augustin.tech/files/beep.mp3")).play();
         clearInterval(intervalId); // Stop the interval
     }
