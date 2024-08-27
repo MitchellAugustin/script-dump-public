35a7f72e76c62a399caa79ac1c32f536927a1040
Mitchell Augustin <mitchell.augustin@canonical.com>
2024-08-27 16:26:29 -0500

    Added ability to run multiple iterations

--- fio-benchmarking/run.sh
+++ fio-benchmarking/run.sh
@@ -1,3 +1,12 @@
-docker run -v std_benchmark_output:/output standard_benchmark:v1
-echo "Output file location:"
-docker volume inspect std_benchmark_output
+if [ $# -eq 0 ]
+  then
+    echo "Usage: $0 <number of iterations>"
+    exit
+fi
+
+for i in $(seq 1 $1);
+do
+	docker run -v std_benchmark_output:/output standard_benchmark:v1
+	echo "Output file location:"
+	docker volume inspect std_benchmark_output
+done
