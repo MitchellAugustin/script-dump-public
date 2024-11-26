76b73985eaa0517f2a73818ef55fd4ef5e3b7039
Mitchell Augustin <mitchell.augustin@canonical.com>
2024-11-26 12:11:04 -0600

    Add working hotplug configs

---
+++ slow-ovmf-case/hotplug-working/attach.sh
@@ -0,0 +1,4 @@
+virsh attach-device 4gpu-vm-2 --file gpu1.xml
+virsh attach-device 4gpu-vm-2 --file gpu2.xml
+virsh attach-device 4gpu-vm-2 --file gpu3.xml
+virsh attach-device 4gpu-vm-2 --file gpu4.xml
